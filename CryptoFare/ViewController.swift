b39243b0a2f3dcd60cdc4ef23c3a5f788433b8c9
Kunal <techcraze5@gmail.com>
2018-09-10 22:45:00 +0530

    Initial Commit

---
+++ CryptoFare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoFare
+//
+//  Created by kunal.ch on 10/09/18.
+//  Copyright © 2018 CryptoFare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
